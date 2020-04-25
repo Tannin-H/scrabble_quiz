48cb253b7a6c3c6ad63094dd0f79ead4d25b9b1a
tanninheeley <tannin.heeley@gmail.com>
2020-04-26 07:59:01 +1200

    the guessing system is now functional and checks each guess against the list of words but has no formated output.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import random
 import string
 
-my_file = open("words", "r")
-
 list_of_lists = []
-potentials = []
-char = []
+my_file = open("words", "r")
 for line in my_file:
     stripped_line = line.strip()
     line_list = stripped_line.split()
@@ -13,8 +10,9 @@ for line in my_file:
 
 my_file.close()
 
-
-def find_words(num_char):
+"""def find_words(num_char):
+    potentials = []
+    char = []
     for list in list_of_lists:
         if len(list[0]) <= num_char:
             potentials.append(list)
@@ -31,34 +29,54 @@ def scrabble_cheater():
     for num in range(0, num_char):
         input_char = input("please enter one of the characters you have available to use ")
         char.append(input_char)
-    find_words()
+    find_words()"""
 
 
 def gen_letters():
     letters_string = string.ascii_lowercase
     letters = list(letters_string)
-    generated_letters = random.choices(letters,
-                                       weights=[5, 1, 1, 1, 5, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1, 1, 1,
-                                                1, 1], k=5)
-    return generated_letters
+    charecters = random.choices(letters,
+                                weights=[5, 1, 1, 1, 5, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 5, 1, 1, 1,
+                                         1, 1], k=5)
+    return charecters
 
 
-def quiz(rand_letters):
+def quiz_main():
+    print()
     print("Thankyou for choosing to partake in the quiz the rules are as follows")
-    print("{} {}".format("the letters you have availabe are ", rand_letters))
+    gen_letters()
+    rand_letters = gen_letters()
+    print("{} {}".format("the letters you have available to use are ", rand_letters))
+    guessing(rand_letters)
 
+
+def guessing(rand_letters):
+    corr_total = 0
+    corr_list = []
+    guess = input("please press enter to begin guessing ")
+    while guess != "f":
+        print()
+        print(rand_letters)
+        guess = input(
+            "please enter you guess and press enter or if you are finished guessing press [f] ").lower().strip()
+        for item in list_of_lists:
+            if guess == item[0]:
+                corr_total += 1
+                corr_list.append(item)
+                print(corr_total)
+    quiz_main()
 
 def main():
     print("welcome to the online word guesser")
-    print("you will be given random letters from the alphabet and the goal is to see how many combinations of words you can "
-        "make")
+    print("you will be given random letters from the alphabet and the goal is to see how many combinations of words "
+          "you are able "
+          "make")
+
     print("if you would like to take part in the quiz press [1]")
     print("if you would like to use the scrabble cheater press [2]")
     choice = int(input("please enter [1 or [2] "))
     if choice == 1:
-        gen_letters()
-        generated_letters = gen_letters()
-        quiz(generated_letters)
+        quiz_main()
 
 
 main()
