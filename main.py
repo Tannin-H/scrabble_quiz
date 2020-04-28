eee7e0fbd7d00f2d1903afc7383519c7fe8c880f
tanninheeley <tannin.heeley@gmail.com>
2020-04-28 13:45:58 +1200

    all of the previous issues fixed and quiz system ready for input validation.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 import string
+import pickle
+from itertools import permutations
 
 list_of_lists = []
 my_file = open("words", "r")
@@ -7,16 +9,29 @@ for line in my_file:
     stripped_line = line.strip()
     line_list = stripped_line.split()
     list_of_lists.append(line_list)
-
 my_file.close()
 
 
 def find_words(char):
-    print(char)
+    possibles = []
+    used = []
+    check = 0
     for item in list_of_lists:
         for letter in item[0]:
-            if letter == char[0]:
-                print(item)
+            if letter in used:
+                check = 0
+            if letter in char:
+                check += 1
+                used.append(letter)
+        if check == len(item[0]):
+            possibles.append(item[0])
+            check = 0
+            used.clear()
+
+        used.clear()
+        check = 0
+    return possibles
+    print(possibles)
 
 
 def scrabble_cheater():
@@ -34,25 +49,32 @@ def gen_letters():
     characters = random.choices(letters,
                                 weights=[2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1,
                                          1, 1], k=10)
+    if "a" not in characters and "e" not in characters and "i" not in characters and "o" not in characters and "u" not in characters:
+        characters.remove(characters[0])
+        characters.append("a")
+        print("added")
     return characters
 
 
 def quiz_main():
+    rand_letters = gen_letters()
+    answers = find_words(rand_letters)
+    print(answers)
     print()
     print("Thank you for choosing to partake in the quiz the rules are as follows")
-    rand_letters = gen_letters()
     print("{} {}".format("the letters you have available to use are ", rand_letters))
-    total, correct = guessing(rand_letters)
+    total, correct = guessing(rand_letters, answers)
     print("{} {}".format("your total score was", total))
     play_again = input("would you like to play again [y]yes or [n]no ")
     if play_again == "y":
         quiz_main()
 
 
-def guessing(rand_letters):
+def guessing(rand_letters, answers):
     corr_total = 0
     corr_list = []
     guessed = []
+    correct = False
     guess = input("please press enter to begin guessing ")
     while guess != "f":
         print()
@@ -60,15 +82,18 @@ def guessing(rand_letters):
         guess = input(
             "please enter you guess and press enter or if you are finished guessing press [f] ").lower().strip()
         guessed.append(guess)
-        for item in list_of_lists:
-            if guess == item[0] and guess != "f":
-                letters = list(guess)
-                find_words(letters)
-                corr_total += len(item[0])
+        for item in answers:
+            if guess == item and guess != "f":
+                answers.remove(item)
+                corr_total += len(item)
                 corr_list.append(item)
                 print("congratulations that was correct ")
                 print("{} {} ".format("The total amount of points you have is", corr_total))
-        print("{} {}".format("the words you previously guessed were", guessed))
+                correct = True
+        if correct == False:
+            print("sorry that guess was incorrect please try again")
+        correct = False
+        print("{} {}".format("the words you have previously guessed are", guessed))
     print()
     return corr_total, corr_list
 
